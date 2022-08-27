d9bfa02d6b370548e3252b62ef755ce3a1b5bc72
Kamil Tomalka <kamil@tomalka.dev>
2022-08-27 09:25:23 +0200

    asdasd

--- index.js
+++ index.js
@@ -81,6 +81,7 @@ http.createServer((req, res) => {
             <html lang="en">
                 <head>
                     <meta charset="utf-8">
+                    <meta name=viewport content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=yes">
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
                     <title>DIRLL - List of Files</title>
